62d73df7494c74cadfca2e012f0f917f02558bc8
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-10-09 21:29:27 -0400

    Got Intents to work.

--- app/src/main/java/com/example/quizz_app/MainActivity.kt
+++ app/src/main/java/com/example/quizz_app/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.quizz_app
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.buttonConfirm)
         button.setOnClickListener {
-            setContentView(R.layout.activity_question2)
+            val intent = Intent(this, Question2::class.java)
+
+            val number = 42
+            intent.putExtra("number_key", number)
+
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/quizz_app/Question2.kt
+++ app/src/main/java/com/example/quizz_app/Question2.kt
@@ -2,10 +2,19 @@ package com.example.quizz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Question2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question2)
+
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+
+        val extras = intent.extras
+        if (extras != null) {
+            val number = extras.getInt("number_key", 0)
+            textViewTitle.text = "You earned: $ $number"
+        }
     }
 }
